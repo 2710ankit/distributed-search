18a94c06f31c284fbb1e50d66524b481060fb999
Ankit Aggarwal <aaggarwal@gammastack.com>
2026-06-25 22:49:58 +0530

    redis added

--- app/services/documents/search_documents.py
+++ app/services/documents/search_documents.py
@@ -1,5 +1,8 @@
 from sqlalchemy.orm import Session
 from app.db.elasticsearch import es
+from app.db.redis import redis_client
+import json
+
 
 
 def search_documents(
@@ -7,6 +10,11 @@ def search_documents(
     tenant_id: str,
     query: str,
 ):
+    redis_key=f"search:{tenant_id}:{query}"
+    redis_data = redis_client.get(redis_key)
+    if redis_data:
+        return json.loads(redis_data)
+
     response = es.search(
         index="documents",
         query={
@@ -34,11 +42,16 @@ def search_documents(
         }
     )
 
-    return [
+    response = [
         {
             "id": hit["_id"],
             **hit["_source"],
             "score": hit["_score"]
-        }
+        }   
         for hit in response["hits"]["hits"]
     ]
+
+    redis_client.set(redis_key, json.dumps(response))
+    redis_client.expire(redis_key, 60 * 60 * 24)
+
+    return response
